54f58764f958433be6b6e2d6faea6cda68672191
Naveed <08es34@gmail.com>
2019-06-25 18:42:26 +0500

    events, styles

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ var commonOverlay = ["Arrow", {foldback: 1, width: 10, length: 15}];
 var commonEndpointOptions = {
     endpoint: ["Rectangle", {
         width: 10,
-        height: 10
+        height: 10,
+        fill: '#00b8ff'
     }],
+    paintStyle: {strokeWidth: 5, stroke: '#00b8ff'},
+    hoverPaintStyle: {strokeWidth: 5, stroke: '#870000'},
     connector: ["Flowchart", {
         stub: 30,
         alwaysRespectStubs: true,
@@ -17,6 +20,16 @@ jsPlumb.bind("ready", function () {
     jsPlumb.draggable(['window1', 'window2', 'window3'], {
         containment: true,
         grid: [10, 10]
+    });
+
+    jsPlumb.bind("connection", function (info, originalEvent) {
+        console.log('connection', info);
+        console.log(originalEvent);
+    });
+
+    jsPlumb.bind("connectionMoved", function (info, originalEvent) {
+        console.log('connectionMoved', info);
+        console.log(originalEvent);
     });
 
     jsPlumb.addEndpoint('window1', {
@@ -46,7 +59,6 @@ jsPlumb.bind("ready", function () {
     jsPlumb.connect({
         source: 'window1',
         target: 'window2',
-        deleteEndpointsOnDetach: false,
         anchors: [[1, 0.75, 1, 0], "Left"],
         overlays: [commonOverlay]
     }, commonEndpointOptions);
